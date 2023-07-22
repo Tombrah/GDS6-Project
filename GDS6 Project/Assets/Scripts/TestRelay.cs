44c0a231830917ab9378e3a7d27f6411e354ae50
Tombrah <bennetttom@optusnet.com.au>
2023-07-23 00:03:29 +1000

    Relay Implemented
    Players can now input the relay code and join another player

--- GDS6 Project/Assets/Scripts/TestRelay.cs
+++ GDS6 Project/Assets/Scripts/TestRelay.cs
@@ -5,9 +5,15 @@ using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using TMPro;
 
 public class TestRelay : MonoBehaviour
 {
+    [SerializeField] private TMP_InputField codeInput;
+
     private async void Start()
     {
         await UnityServices.InitializeAsync();
@@ -25,6 +31,32 @@ public class TestRelay : MonoBehaviour
         try
         {
             Allocation allocation = await RelayService.Instance.CreateAllocationAsync(1);
+
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            Debug.Log(joinCode);
+
+            RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+
+            NetworkManager.Singleton.StartHost();
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    public async void JoinRelay()
+    {
+        try
+        {
+            string joinCode = codeInput.text;
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+
+            RelayServerData relayServerData = new RelayServerData(joinAllocation, "dtls");
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+
+            NetworkManager.Singleton.StartClient();
         }
         catch (RelayServiceException e)
         {
