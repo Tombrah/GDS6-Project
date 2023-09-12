bf7357b8fad87c600c374e1546503f6ab427a648
MASKKED <110505717+MASKKED@users.noreply.github.com>
2023-09-12 14:13:38 +1000

    Sniff Ability

---
+++ GDS6 Project/Assets/Scripts/Abilities/TrailFader.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using System.Collections;
+
+public class TrailFader : MonoBehaviour
+{
+    public Material targetMaterial;
+    private bool isFading = false;
+    private float fadeDuration = 1.0f;
+    private float maxAlpha = 1.0f;
+    private float currentAlpha = 0.0f;
+    private bool isVisible = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G) && !isFading)
+        {
+            StartFadeIn();
+        }
+    }
+
+    private void StartFadeIn()
+    {
+        isFading = true;
+        StartCoroutine(FadeIn());
+    }
+
+    private IEnumerator FadeIn()
+    {
+        float elapsedTime = 0;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+
+            currentAlpha = Mathf.Lerp(0.0f, maxAlpha, elapsedTime / fadeDuration);
+
+            Color currentColor = targetMaterial.color;
+            currentColor.a = currentAlpha;
+            targetMaterial.color = currentColor;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        // Material is fully visible after fading in.
+        isVisible = true;
+
+        // Wait for 5 seconds before fading out.
+        yield return new WaitForSeconds(5.0f);
+
+        // Start fading out.
+        StartFadeOut();
+    }
+
+    private void StartFadeOut()
+    {
+        StartCoroutine(FadeOut());
+    }
+
+    private IEnumerator FadeOut()
+    {
+        float elapsedTime = 0;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            currentAlpha = Mathf.Lerp(maxAlpha, 0.0f, elapsedTime / fadeDuration);
+
+            Color currentColor = targetMaterial.color;
+            currentColor.a = currentAlpha;
+            targetMaterial.color = currentColor;
+
+            yield return null;
+        }
+
+        // Material is fully transparent after fading out.
+        isVisible = false;
+        isFading = false;
+    }
+}
