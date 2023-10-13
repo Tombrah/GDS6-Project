483175a6698cc0ae3f84b9648d9345f5795e6cb3
Tombrah <bennetttom@optusnet.com.au>
2023-10-13 18:12:36 +1100

    Name Limited to 16 characters. added inspector button to reset playerdata

--- GDS6 Project/Assets/Scripts/Extra/MainMenuCleanup.cs
+++ GDS6 Project/Assets/Scripts/Extra/MainMenuCleanup.cs
@@ -5,21 +5,11 @@ using Unity.Netcode;
 
 public class MainMenuCleanup : MonoBehaviour
 {
-    public bool ResetPlayerData;
-
     private void Awake()
     {
         if (NetworkManager.Singleton != null)
         {
             Destroy(NetworkManager.Singleton.gameObject);
-        }
-    }
-
-    private void Start()
-    {
-        if (ResetPlayerData)
-        {
-            PlayerPrefs.DeleteAll();
         }
     }
 }
