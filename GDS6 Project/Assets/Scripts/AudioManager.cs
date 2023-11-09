519bbe24d77fe82eef87cc5c5299c98985ebd5cc
Tombrah <bennetttom@optusnet.com.au>
2023-11-09 12:44:19 +1100

    No BGM

--- GDS6 Project/Assets/Scripts/AudioManager.cs
+++ GDS6 Project/Assets/Scripts/AudioManager.cs
@@ -6,7 +6,6 @@ public class AudioManager : MonoBehaviour
 
 {
     public AudioSource roundsound;
-    public AudioSource BGM;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,20 +17,7 @@ public class AudioManager : MonoBehaviour
         if(GameManager.Instance.IsCountdownActive())
         {
             roundsound.Play();
-        }
-        else if(GameManager.Instance.IsGamePlaying())
-        {
-            if(!BGM.isPlaying)
-            {
-                BGM.Play();
-            }
-            
-        }    
+        } 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
